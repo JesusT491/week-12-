a0e6e4616ea397a2c51e709959f9d540bf4b36c1
JesusT491 <jtorres217@cps.edu>
2025-11-14 20:59:24 +0000

    2

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -9,33 +9,95 @@
 # lists are mutable, meaning you can change their content
 # lists are created using square brackets []
 
-my_list = [1,2,3,4,5]
-print(my_list) # [1,2,3,4,5]
-print(type(my_list)) # <class 'list'
-
-print(my_list[0])
-print(my_list[1:4])
-print(my_list[0:])
-
-#to add items to a list use .append() and or .extend()
-
-my_list.append(6) #singular values
-print(my_list)
-
-my_list.extend([7, 8]) #multiple values
-print(my_list)
-
-my_list.extend(list(range(5,515))) #mutliple values with a scope of numbers
-print(my_list)
-
-my_list.extend(list(range(600, 1000))) #example
-print(my_list)
-
-
 #instead of creating a whole bunch of seperate variables, you can instead put all those variables in a list so that you can access them later.
 # efficient for objects that are similar
 # you can change all those objects all at once
 # makes our job easier
+
+
+# my_list = [1,2,3,4,5]
+# print(my_list) # [1,2,3,4,5]
+# print(type(my_list)) # <class 'list'
+
+# print(my_list[0])
+# print(my_list[1:4])
+# print(my_list[0:])
+
+# #to add items to a list use .append() and or .extend()
+
+# my_list.append(6) #singular values
+# print(my_list)
+
+# my_list.extend([7, 8]) #multiple values
+# print(my_list)
+
+# my_list.extend(list(range(5,515))) #mutliple values with a scope of numbers
+# print(my_list)
+
+# my_list.extend(list(range(600, 1000))) #example
+# print(my_list)
+
+#-----
+
+new_list = ['a','b','c']
+print(new_list)
+new_list.append('d')
+print(new_list)
+
+#removing an item from the list at the end by default
+removed_item = new_list.pop()
+
+print(removed_item)
+print(new_list)
+#removes the first item from the list 
+removed_second_item = new_list.pop(1)
+print(removed_second_item)
+print(new_list)
+#sorts items
+
+numbers = [4,2,5,1,3]
+numbers.sort()
+print(numbers)
+
+#reverses list
+
+numbers.reverse()
+print(numbers)
+
+#inserts items into specific position
+
+numbers.insert(2,10)
+print(numbers)
+
+# replaces values inside the list with other values
+third_list = [7,8,9]
+third_list[0] = 6
+print(third_list)
+third_list[-1] = 10
+print(third_list)
+
+#prints a list of 10 random values ranging from 1 to 100 
+import random
+
+random_list = random.sample(range(1,1000), 100)
+print(random_list)
+print(sorted(random_list))
+
+sorted_list = sorted(random_list)
+print(sorted_list)
+
+#removes every third value in the list
+
+list = sorted_list.reverse
+list.pop(3)
+print(list)
+
+#summary of list functions
+# .append(item) - adds an item to the end of the list
+# .pop(index) - removes and returns the item at the specificed index
+# .sort() - sorts the list in asecending order
+# .reverse() - reverses the order of the list
+
 
 
 # Examples:
