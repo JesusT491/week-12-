a0e2c0dd962a60355ce15d82cb320856c3a851f9
JesusT491 <jtorres217@cps.edu>
2025-11-18 20:03:55 +0000

    commit1

---
+++ sets_tuples.py
@@ -0,0 +1,24 @@
+# sets and tuples and lists examples:
+
+#sets use {}
+#sets are not able to be duplicated or ordered
+
+set1 ={1,2,3,4,5}
+print(set1)
+print(type(set1))
+set1.add(6)
+print(set1)
+set1.remove(2)
+print(set1)
+
+# sets remove duplicate items in the array
+set2 = {"apple", "banana", "cherry", "cherry"}
+print(set2)
+
+#tuples
+tuple1 = (1,2,3,4,5)
+print(tuple1)
+print(type(tuple1))
+
+social_security_number = (123444,4444445,5676789)
+print(social_security_number)
